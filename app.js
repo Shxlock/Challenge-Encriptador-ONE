33035b93c4c4b261b892e714b30e681395586aec
Shxlock <albarracinjuancarlos800@gmail.com>
2024-07-05 12:43:04 -0500

    Mas personalizacion

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function encriptar() {
   let textoIngresado = document.getElementById("texto").value;
   let tituloMensaje = document.getElementById("titulo-mensaje");
@@ -17,27 +16,23 @@ function encriptar() {
     tituloMensaje.textContent = "Texto encriptado con éxito";
     document.getElementById("texto").value = "";
     
-    
-    if (muñeco) {
-      muñeco.parentNode.removeChild(muñeco);
-    }
+    muñeco.src = "./assets/candado-cerrado.png";
 
     let mensajeEncriptadoDiv = document.querySelector('.mensaje-encriptado');
-    
     
     let botonCopiar = document.createElement('button');
     botonCopiar.classList.add('btn-copiar');
     botonCopiar.textContent = 'Copiar Texto';
     botonCopiar.onclick = copiarTexto;
-    
-   
     mensajeEncriptadoDiv.appendChild(botonCopiar);
-    
   } else {
     tituloMensaje.textContent = "Ningún mensaje fue encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     swal("Ooops!", "Debes ingresar un texto", "warning");
   }
+  
+
+
 }
 
 function desencriptar() {
@@ -57,36 +52,27 @@ function desencriptar() {
     parrafo.textContent = textoDesencriptado;
     tituloMensaje.textContent = "Texto desencriptado con éxito";
     document.getElementById("texto").value = "";
-    
-    if (muñeco) {
-      muñeco.parentNode.removeChild(muñeco);
-    }
+    muñeco.src = "/assets/candado-abierto.png"
 
     let botonCopiar = document.querySelector('.btn-copiar');
     if (botonCopiar) {
       botonCopiar.parentNode.removeChild(botonCopiar);
     }
+
   } else {
     tituloMensaje.textContent = "Ningún mensaje fue encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     swal("Ooops!", "Debes ingresar un texto", "warning");
   }
+  
+
 }
 
 function copiarTexto() {
   let texto = document.getElementById("parrafo").textContent;
   if (texto !== "") {
     navigator.clipboard.writeText(texto)
-      .then(() => {
-        swal("Texto Copiado!", "El texto ha sido copiado al portapapeles.", "success");
-      })
-      .catch(err => {
-        console.error('Error al copiar el texto: ', err);
-        swal("Error!", "Hubo un problema al intentar copiar el texto.", "error");
-      });
+    swal("¡Texto Copiado!", "El texto ha sido copiado al portapapeles.", "success"); 
   }
 }
-
-
-
 
